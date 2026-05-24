7f53df8c3781014f4441f2c8c71bdbf6b8ec8d39
Malik3112008 <maf213413@gmail.com>
2026-05-24 19:31:56 +0700

    Update mobile app to version 1.0.2

--- app/components/InstallGuideSection.tsx
+++ app/components/InstallGuideSection.tsx
@@ -39,7 +39,7 @@ const platforms: PlatformData[] = [
     label: "Android",
     icon: Smartphone,
     downloadLabel: "Download APK",
-    downloadHref: "/sas-mobile-v1.0.1.apk",
+    downloadHref: "/sas-mobile-v1.0.2.apk",
     steps: [
       {
         title: "Download APK atau buka Google Play",
